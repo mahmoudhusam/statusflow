2ec506112bb5694c621d464c0e44539b59ce424b
Mahmoud Ayesh <mahayesh7@gmail.com>
2025-12-15 13:11:18 +0200

    updated the Cors

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -9,7 +9,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3001',
+    origin: process.env.FRONTEND_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
